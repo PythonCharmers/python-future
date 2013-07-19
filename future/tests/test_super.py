087a065ddc4aa9762b2b0e0e6b74b5ef71a0797f
Ed Schofield <ed@pythoncharmers.com>
2013-07-19 17:41:42 +1000

    Add Python 3-compatible super() function
    This is a version of Ryan Kelly's magicsuper module that is compatible
    with Python 3 and doesn't modify __builtin__

---
+++ future/tests/test_super.py
@@ -0,0 +1,27 @@
+'''
+Tests for the new super() function syntax
+'''
+from __future__ import absolute_import, print_function
+from future import super
+
+import unittest
+
+
+class TestSuper(unittest.TestCase):
+    def test_super(self):
+        class verbose_list(list):
+            '''
+            A class that uses the new simpler super() function
+            '''
+            def append(self, item):
+                print('Adding an item')
+                super().append(item)
+
+        l = verbose_list()
+        l.append('blah')
+        self.assertEqual(l[0], 'blah')
+        self.assertEqual(len(l), 1)
+        self.assertTrue(isinstance(l, list))
+
+
+unittest.main()
