ee5c6ea1b4961c071438c3cdc8d6b9bd9b370c5c
Ed Schofield <ed@pythoncharmers.com>
2013-07-22 19:30:58 +1000

    Bump version number to 0.1.0
    - All the tests pass now for the first time. Hooray!

--- future/__init__.py
+++ future/__init__.py
@@ -45,8 +45,8 @@ from future.str_is_unicode import *
 
 
 __ver_major__ = 0
-__ver_minor__ = 0
-__ver_patch__ = 3
+__ver_minor__ = 1
+__ver_patch__ = 0
 __ver_sub__ = ''
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
 VERSION = __version__
