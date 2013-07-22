054071401a41704b3035af0cbdf029e05c80303c
Ed Schofield <ed@pythoncharmers.com>
2013-07-22 15:17:51 +1000

    Clean up and simplify standard_library_renames

--- future/standard_library_renames.py
+++ future/standard_library_renames.py
@@ -7,20 +7,37 @@ It is designed to be used as follows:
 
     from future import standard_library_renames
 
-And then, for example:
+And then these imports work:
 
     import builtins
     import configparser
     import copyreg
+    import io
+    import _markupbase
     import queue
+    import reprlib
     import socketserver
     import tkinter
+    import winreg (on Windows only)
+
+The modules are still available under their old names on Python 2.
 
 We don't currently support these, but would like to:
 
+    import pickle (should bring in cPickle on Python 2)
     import http.cookies
+    import http.cookiejar
     import http.server
+    import http.client
+    import dbm
+    import dbm.dumb
+    import dbm.gnu
+    import urllib.request
     import urllib.parse
+    import urllib.error
+    import xmlrpc.client
+    import urllib.robotparser
+    import test.support
     import xmlrpc.client
 
 This module only supports Python 2.7 and Python 3.1+.
@@ -28,43 +45,28 @@ This module only supports Python 2.7 and Python 3.1+.
 
 from __future__ import absolute_import, print_function
 
-import inspect
-import importlib
 import sys
-import warnings
 import logging
 import imp
 
-from . import six
+# A subset of six.moves:
+RENAMES = {
+           # 'anydbm': 'dbm',   # causes infinite import loop 
+           # 'whichdb': 'dbm',  # causes infinite import loop 
+           'ConfigParser': 'configparser',
+           'copy_reg': 'copyreg',
+           'cPickle': 'pickle',
+           'cProfile': 'profile',
+           'cStringIO': 'io',
+           'markupbase': '_markupbase',
+           'Queue': 'queue',
+           'repr': 'reprlib',
+           'SocketServer': 'socketserver',
+           'Tkinter': 'tkinter',
+           '_winreg': 'winreg',
+           '__builtin__': 'builtins',
+          }
 
-# mapping = {'ConfigParser': 'configparser',
-#            'Queue': 'queue',
-#            'SocketServer': 'socketserver',
-#            'Tkinter': 'tkinter',
-#            '__builtin__': 'builtins',
-#            'copy_reg': 'copyreg'}
-
-mapping = {thing.mod: thing.name for thing in six._moved_attributes \
-           if isinstance(thing, six.MovedModule)}
-
-
-
-# class ImportBlocker(object):
-#     def __init__(self, *args):
-#         self.module_names = args
-#     
-#     def find_module(self, fullname, path=None):
-#         if fullname in self.module_names:
-#             return self
-#         return None
-#     
-#     def load_module(self, name):
-#         raise ImportError("%s is blocked and cannot be imported" % name)
-# 
-# print(sys.meta_path)
-# sys.meta_path = [ImportBlocker('ConfigParser')]
-
-	
 
 class WarnOnImport(object):
     def __init__(self, *args):
@@ -85,27 +87,46 @@ class WarnOnImport(object):
  
         logging.warning("Imported deprecated module %s", name)
         return module
+
+
+class RenameImport(object):
+    def __init__(self, old_to_new):
+        '''
+        Pass in a dictionary-like object mapping from old names to new
+        names. E.g. {'ConfigParser': 'configparser', 'cPickle': 'pickle'}
+        '''
+        self.old_to_new = old_to_new
+        both = set(old_to_new.keys()) & set(old_to_new.values())
+        # print(both)
+        assert len(both) == 0, 'Ambiguity in renaming (handler not implemented'
+        self.new_to_old = {new: old for (old, new) in old_to_new.items()}
+ 
+    def find_module(self, fullname, path=None):
+        if fullname in set(self.old_to_new) | set(self.new_to_old):
+            self.path = path
+            return self
+        return None
+ 
+    def load_module(self, name):
+        if name in sys.modules:
+            return sys.modules[name]
+        if name in self.new_to_old:
+            # New name
+            oldname = self.new_to_old[name]
+            module_info = imp.find_module(oldname, self.path)
+            module = imp.load_module(oldname, *module_info)
+        elif name in self.old_to_new:
+            # Old name. Import with warning.
+            module_info = imp.find_module(name, self.path)
+            module = imp.load_module(name, *module_info)
+            logging.warning("Imported deprecated module %s", name)
+        else: 
+            # Something else
+            module_info = imp.find_module(name, self.path)
+            module = imp.load_module(name, *module_info)
+        sys.modules[name] = module
+        return module
  
 
-sys.meta_path = [WarnOnImport('getopt', 'optparse', # etc.
-                                 )]
+sys.meta_path = [RenameImport(RENAMES)]
 
-
-if not six.PY3:
-    for oldname, newname in mapping.items():
-        # print('Importing module ' + oldname)
-        try:
-            module = importlib.import_module(oldname, package=None)
-        except ImportError as e:
-            # Expected to fail (on non-Windows ...)
-            if not oldname == '_winreg':
-                warnings.warn('Could not import module ' + oldname)
-        else:
-            sys.modules[newname] = module
-        sys.modules[oldname] = None
-        # module.__name__ = newname   # has no effect?!
-
-    caller = inspect.currentframe().f_back
-    # caller.f_globals[newname] = oldname
-
-# print(len(sys.modules))
