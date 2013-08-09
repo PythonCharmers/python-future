2be50543ad7c5239dbf00241334e88e4fe11bb1d
Ed Schofield <ed@pythoncharmers.com>
2013-08-10 01:09:58 +1000

    Bump version to 0.4.0-rc

--- future/__init__.py
+++ future/__init__.py
@@ -173,7 +173,7 @@ else:
 __ver_major__ = 0
 __ver_minor__ = 4
 __ver_patch__ = 0
-__ver_sub__ = '-pre'
+__ver_sub__ = '-rc'
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
 
 
