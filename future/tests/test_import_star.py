3f72acf66d336d4823318ef089e46052d1a6fe64
Ed Schofield <ed@pythoncharmers.com>
2013-10-01 11:27:16 +1000

    Remove last vestiges of ``from future import *`` from code and comments

--- future/tests/test_import_star.py
+++ future/tests/test_import_star.py
@@ -1,15 +1,12 @@
 """
 This tests whether
 
-    from future import *
-
-and
-
     from future.builtins import *
 
-work as expected. They should NOT introduce namespace pollution. On Python 3,
-this should have precisely no effect whatsoever. On Python 2, this should not
-introduce any new symbols, but merely shadow some of the builtins.
+works as expected:
+- This should NOT introduce namespace pollution on Py3.
+- On Python 2, this should not introduce any symbols that aren't in
+  __builtin__.
 
 """
 
@@ -23,7 +20,6 @@ from future import utils
 original_locals = set(copy.copy(locals()))
 original_globals = set(copy.copy(globals()))
 new_names = {'original_locals', 'original_globals', 'new_names'}
-from future import *
 from future.builtins import *
 new_locals = set(copy.copy(locals())) - new_names - original_locals
 new_globals = set(copy.copy(globals())) - new_names - original_globals - \
--- libfuturize/fixes3/__init__.py
+++ libfuturize/fixes3/__init__.py
@@ -16,7 +16,7 @@ libfuturize_3fix_names = set([
                             # 'libfuturize.fixes3.fix_features',  # ?
                             'libfuturize.fixes3.fix_fullargspec',
                             # 'libfuturize.fixes3.fix_funcattrs',
-                            'libfuturize.fixes3.fix_future_builtins',   # adds "from future import *"
+                            'libfuturize.fixes3.fix_future_builtins',   # adds "from future.builtins import *"
                             'libfuturize.fixes3.fix_getcwd',
                             'libfuturize.fixes3.fix_imports',   # adds "from future import standard_library"
                             'libfuturize.fixes3.fix_imports2',
