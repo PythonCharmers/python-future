ed11fa0ebc365b8a7b0f31c8b09bf23b891e44b6
Ed Schofield <ed@pythoncharmers.com>
2013-10-25 15:36:06 +1100

    Allow test discovery on Py26 with unittest2

--- discover_tests.py
+++ discover_tests.py
@@ -7,6 +7,9 @@ import os
 import sys
 import unittest
 
+if not hasattr(unittest.defaultTestLoader, 'discover'):
+    import unittest2 as unittest
+
 def additional_tests():
     setup_file = sys.modules['__main__'].__file__
     setup_dir = os.path.abspath(os.path.dirname(setup_file))
