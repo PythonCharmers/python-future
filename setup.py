b9ac1d6062c8b9f33ca3ccf101f3ac91b5916d63
Ed Schofield <ed@pythoncharmers.com>
2013-11-08 15:56:00 +1100

    Add Python 2.6 to PyPI classifier metadata

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ LICENSE = "MIT"
 KEYWORDS = "future python3 migration backport six 2to3 futurize modernize"
 CLASSIFIERS = [
     "Programming Language :: Python",
+    "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.3",
