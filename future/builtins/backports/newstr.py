05bb9e11d71a7cc3bc6863af1fa01d980c7ea331
Ed Schofield <ed@pythoncharmers.com>
2013-11-12 08:02:47 +1100

    Test and fix for issue #15: os.path.join(p, s) with newstr

--- future/builtins/backports/newstr.py
+++ future/builtins/backports/newstr.py
@@ -123,18 +123,17 @@ class newstr(with_metaclass(BaseNewStr, unicode)):
             raise TypeError(errmsg.format(type(key)))
         return issubset(list(newkey), list(self))
     
-    @no(bytes)
+    @no(newbytes)
     def __add__(self, other):
         return newstr(super(newstr, self).__add__(other))
 
-    @no(bytes)
+    @no(newbytes)
     def __radd__(self, left):
         " left + self "
-        if isinstance(left, unicode):
-            try:
-                return newstr(left) + self
-            except:
-                raise NotImplemented
+        try:
+            return newstr(left) + self
+        except:
+            return NotImplemented
 
     def __mul__(self, other):
         return newstr(super(newstr, self).__mul__(other))
@@ -149,15 +148,15 @@ class newstr(with_metaclass(BaseNewStr, unicode)):
                 raise TypeError(errmsg.format(i))
         return newstr(super(newstr, self).join(iterable))
 
-    @no(bytes)
+    @no(newbytes)
     def find(self, sub, *args):
         return super(newstr, self).find(sub, *args)
 
-    @no(bytes)
+    @no(newbytes)
     def rfind(self, sub, *args):
         return super(newstr, self).rfind(sub, *args)
 
-    @no(bytes, (1, 2))
+    @no(newbytes, (1, 2))
     def replace(self, old, new, *args):
         return newstr(super(newstr, self).replace(old, new, *args))
 
@@ -179,19 +178,19 @@ class newstr(with_metaclass(BaseNewStr, unicode)):
         # not keyword arguments as in Python 3 str.
         return newbytes(super(newstr, self).encode(encoding, errors))
 
-    @no(bytes, 1)
+    @no(newbytes, 1)
     def startswith(self, prefix, *args):
         if isinstance(prefix, Iterable):
             for thing in prefix:
-                if not isinstance(thing, unicode):
+                if type(thing) == newbytes:
                     raise TypeError(self.no_convert_msg.format(type(thing)))
         return super(newstr, self).startswith(prefix, *args)
 
-    @no(bytes, 1)
+    @no(newbytes, 1)
     def endswith(self, prefix, *args):
         if isinstance(prefix, Iterable):
             for thing in prefix:
-                if not isinstance(thing, unicode):
+                if type(thing) == newbytes:
                     raise TypeError(self.no_convert_msg.format(type(thing)))
         return super(newstr, self).endswith(prefix, *args)
 
@@ -203,7 +202,7 @@ class newstr(with_metaclass(BaseNewStr, unicode)):
         parts = super(newstr, self).split(sep, maxsplit)
         return [newstr(part) for part in parts]
 
-    @no(bytes, 1)
+    @no(newbytes, 1)
     def rsplit(self, sep=None, maxsplit=-1):
         # Py2 unicode.rsplit() takes maxsplit as an optional parameter,
         # not as a keyword argument as in Python 3 str.
@@ -212,17 +211,17 @@ class newstr(with_metaclass(BaseNewStr, unicode)):
         parts = super(newstr, self).rsplit(sep, maxsplit)
         return [newstr(part) for part in parts]
 
-    @no(bytes, 1)
+    @no(newbytes, 1)
     def partition(self, sep):
         parts = super(newstr, self).partition(sep)
         return tuple(newstr(part) for part in parts)
 
-    @no(bytes, 1)
+    @no(newbytes, 1)
     def rpartition(self, sep):
         parts = super(newstr, self).rpartition(sep)
         return tuple(newstr(part) for part in parts)
 
-    @no(bytes, 1)
+    @no(newbytes, 1)
     def index(self, sub, *args):
         """
         Like newstr.find() but raise ValueError when the substring is not
