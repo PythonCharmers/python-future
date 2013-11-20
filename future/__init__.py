8941241adb84d00a7eb6eac75a4b2579bdaeb5a8
Ed Schofield <ed@pythoncharmers.com>
2013-11-21 09:14:30 +1100

    Bump version to v0.10.0-dev

--- future/__init__.py
+++ future/__init__.py
@@ -70,8 +70,8 @@ The software is distributed under an MIT licence. See LICENSE.txt.
 __all__ = []
 
 __ver_major__ = 0
-__ver_minor__ = 9
-__ver_patch__ = 1
+__ver_minor__ = 10
+__ver_patch__ = 0
 __ver_sub__ = '-pre'
 __version__ = "%d.%d.%d%s" % (__ver_major__, __ver_minor__,
                               __ver_patch__, __ver_sub__)
