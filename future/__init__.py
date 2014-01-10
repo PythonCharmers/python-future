dc2d0f66388006b191bea31029d660de185e796d
Ed Schofield <ed@pythoncharmers.com>
2014-01-11 10:35:53 +1100

    Bump version to v0.10.2

--- future/__init__.py
+++ future/__init__.py
@@ -62,7 +62,7 @@ __all__ = []
 
 __ver_major__ = 0
 __ver_minor__ = 10
-__ver_patch__ = 1
+__ver_patch__ = 2
 __ver_sub__ = ''
 __version__ = "%d.%d.%d%s" % (__ver_major__, __ver_minor__,
                               __ver_patch__, __ver_sub__)
