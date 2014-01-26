fc89769dab0876f117d587aa5d31dcb0329ed949
Ed Schofield <ed@pythoncharmers.com>
2014-01-26 19:19:31 +1100

    Add a pasteurize.py script

---
+++ pasteurize.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+pasteurize.py
+=============
+
+Like Armin Ronacher's ``modernize.py``, but using the ``future`` package rather than a direct dependency on ``six``'.
+
+pasteurize.py attempts to turn Py3 code into relatively clean Py3 code that is also
+compatible with Py2 when using the ``future`` package.
+
+
+Licensing
+---------
+Copyright 2013-2014 Python Charmers Pty Ltd, Australia.
+The software is distributed under an MIT licence. See LICENSE.txt.
+"""
+
+import os
+
+from libpasteurize.main import main
+
+# We use os._exit() because sys.exit() seems to interact badly with
+# subprocess.check_output() ...
+os._exit(main())
+
