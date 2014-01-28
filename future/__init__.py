c05d809e0a987faab831447f8c670bcf0c499b12
Ed Schofield <ed@pythoncharmers.com>
2014-01-28 16:50:37 +1100

    Bump version to v0.11

--- future/__init__.py
+++ future/__init__.py
@@ -81,6 +81,6 @@ __copyright__ = 'Copyright 2014 Python Charmers Pty Ltd'
 __ver_major__ = 0
 __ver_minor__ = 11
 __ver_patch__ = 0
-__ver_sub__ = '-dev'
+__ver_sub__ = ''
 __version__ = "%d.%d.%d%s" % (__ver_major__, __ver_minor__,
                               __ver_patch__, __ver_sub__)
