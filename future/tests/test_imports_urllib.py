e52b58e823e803c0cce47a1e24e41577088a25a0
Ed Schofield <ed@pythoncharmers.com>
2014-02-24 19:10:03 +1100

    Add a new little urllib import test

---
+++ future/tests/test_imports_urllib.py
@@ -0,0 +1,15 @@
+import unittest
+import sys
+print([m for m in sys.modules if m.startswith('urllib')])
+
+class MyTest(unittest.TestCase):
+    def test_urllib(self):
+        import urllib
+        print(urllib.__file__)
+        from future import standard_library
+        with standard_library.hooks():
+            import urllib.response
+        print(urllib.__file__)
+        print(urllib.response.__file__)
+
+unittest.main()
