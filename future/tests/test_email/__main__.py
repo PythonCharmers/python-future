717c1b25fee351ffa66c8417ca3f1df2491686c3
Ed Schofield <ed@pythoncharmers.com>
2014-03-10 18:27:07 +1100

    Add ``pasteurize``d email tests

---
+++ future/tests/test_email/__main__.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_hooks()
+from test.test_email import test_main
+
+test_main()
