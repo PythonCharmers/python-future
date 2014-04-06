f8074d60f00a6dce06042ad8f49d5d9c5f02042c
Ed Schofield <ed@pythoncharmers.com>
2014-04-06 12:46:34 +1000

    Make newlist a subclass of newobject

--- future/builtins/types/newlist.py
+++ future/builtins/types/newlist.py
@@ -16,17 +16,18 @@ import sys
 import copy
 
 from future.utils import with_metaclass
+from future.builtins.types.newobject import newobject, BaseNewObject
 
 
 _builtin_list = list
 ver = sys.version_info[:2]
 
 
-class BaseNewList(type):
+class BaseNewList(BaseNewObject):
     def __instancecheck__(cls, instance):
         return isinstance(instance, _builtin_list)
 
-class newlist(with_metaclass(BaseNewList, _builtin_list)):
+class newlist(with_metaclass(BaseNewList, _builtin_list, newobject)):
     """
     A backport of the Python 3 list object to Py2
     """
