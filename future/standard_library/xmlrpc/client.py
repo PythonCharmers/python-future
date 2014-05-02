982127f41bea516e2b153d851b940b0a2eac1fbe
Ed Schofield <ed@pythoncharmers.com>
2014-05-02 19:48:38 +1000

    Revert "Fix another couple of repr tests"
    This reverts commit a43e8bf91ab038dd73e6f4b464d9d6f01fc93ea0.

--- future/standard_library/xmlrpc/client.py
+++ future/standard_library/xmlrpc/client.py
@@ -243,8 +243,8 @@ class Fault(Error):
     """Indicates an XML-RPC fault package."""
     def __init__(self, faultCode, faultString, **extra):
         Error.__init__(self)
-        self.faultCode = str(faultCode)
-        self.faultString = str(faultString)
+        self.faultCode = faultCode
+        self.faultString = faultString
     def __repr__(self):
         return "<Fault %s: %r>" % (self.faultCode, self.faultString)
 
@@ -309,7 +309,7 @@ class DateTime(object):
 
     def __init__(self, value=0):
         if isinstance(value, str):
-            self.value = str(value)
+            self.value = value
         else:
             self.value = _strftime(value)
 
