c289c2da821834f7526e7a209d83d402cd58bdd5
Ed Schofield <ed@pythoncharmers.com>
2014-10-02 23:23:12 +1000

    Docs: Add instructions for uploading PDF cheatsheet

--- docs/other/upload_future_docs.sh
+++ docs/other/upload_future_docs.sh
@@ -4,6 +4,7 @@ On the local machine
 git checkout v0.7.0
 rm -Rf docs/build/
 cd docs; make html
+cp cheatsheet.pdf /shared/
 cd build
 touch /shared/python-future-html-docs.zip
 rm /shared/python-future-html-docs.zip
@@ -11,6 +12,7 @@ zip -r /shared/python-future-html-docs.zip *
 
 cd /shared
 scp python-future-html-docs.zip python-future.org:
+scp cheatsheet.pdf python-future.org:
 ssh python-future.org
 
 
@@ -20,4 +22,6 @@ On the remote machine:
 cd /var/www/python-future/
 unzip -o ~/python-future-html-docs.zip
 chmod a+r * html/* html/_static/*
+cp ~/cheatsheet.pdf ./html/compatible-idioms.pdf
+cp ~/cheatsheet.pdf ./html/cheatsheet.pdf
 
