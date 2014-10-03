b632792d8a2b7d8bf4d920b0fd83465af0745e23
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2014-10-02 22:05:15 -0400

    Make html.escape handle both bytes / string.

--- src/html/__init__.py
+++ src/html/__init__.py
@@ -14,18 +14,22 @@ else:
 
 
     _escape_map = {ord('&'): '&amp;', ord('<'): '&lt;', ord('>'): '&gt;'}
+    _escape_map_u = {k: unicode(v) for k, v in _escape_map.items()}
     _escape_map_full = {ord('&'): '&amp;', ord('<'): '&lt;', ord('>'): '&gt;',
                         ord('"'): '&quot;', ord('\''): '&#x27;'}
-
-    # NB: this is a candidate for a bytes/string polymorphic interface
+    _escape_map_full_u = {k: unicode(v) for k, v in _escape_map_full.items()}
 
     def escape(s, quote=True):
         """
         Replace special characters "&", "<" and ">" to HTML-safe sequences.
         If the optional flag quote is true (the default), the quotation mark
-        characters, both double quote (") and single quote (') characters are also
-        translated.
+        characters, both double quote (") and single quote (') characters are
+        also translated.
         """
         if quote:
-            return s.translate(_escape_map_full)
-        return s.translate(_escape_map)
+            return s.translate(_escape_map_full
+                               if isinstance(s, str)
+                               else _escape_map_full_u)
+        return s.translate(_escape_map
+                           if isinstance(s, str)
+                           else _escape_map_u)
