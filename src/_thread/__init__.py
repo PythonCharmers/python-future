09e17d4071e09cea67f52f3141938c8a7636ab34
Ed Schofield <ed@pythoncharmers.com>
2014-12-14 18:06:34 +1100

    Remove import of dummy_thread from _thread module (issue #124)

--- src/_thread/__init__.py
+++ src/_thread/__init__.py
@@ -3,10 +3,7 @@ import sys
 __future_module__ = True
 
 if sys.version_info[0] < 3:
-    try:
-        from thread import *
-    except ImportError:
-        from dummy_thread import *
+    from thread import *
 else:
     raise ImportError('This package should not be accessible on Python 3. '
                       'Either you are trying to run from the python-future src folder '
