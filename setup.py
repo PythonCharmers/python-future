2c84ded5494ce22f4de15966f9a840d94945047e
Anika Mukherji <amukherji@pinterest.com>
2019-09-27 11:51:15 -0700

    drop py 2.6

--- setup.py
+++ setup.py
@@ -95,7 +95,6 @@ KEYWORDS = "future past python3 migration futurize backport six 2to3 modernize p
 CLASSIFIERS = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.3",
@@ -175,7 +174,7 @@ setup(name=NAME,
       packages=PACKAGES,
       package_data=PACKAGE_DATA,
       include_package_data=True,
-      python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*",
+      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*",
       classifiers=CLASSIFIERS,
       **setup_kwds
      )
