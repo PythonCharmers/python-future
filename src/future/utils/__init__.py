08dec19511b7fdae5d24e7ff8bfbeba0fccdbd18
Anika Mukherji <amukherji@pinterest.com>
2019-09-27 09:03:50 -0700

    minor change to run travis again

--- src/future/utils/__init__.py
+++ src/future/utils/__init__.py
@@ -580,6 +580,7 @@ def isnewbytes(obj):
     Equivalent to the result of ``type(obj)  == type(newbytes)``
     in other words, it is REALLY a newbytes instance, not a Py2 native str
     object?
+
     Note that this does not cover subclasses of newbytes, and it is not
     equivalent to ininstance(obj, newbytes)
     """
