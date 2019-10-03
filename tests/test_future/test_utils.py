600344df9288810bf5092336e17abfc80d2e6665
Anika Mukherji <amukherji@pinterest.com>
2019-10-02 23:27:07 -0700

    attempt to fix another py2.6 test

--- tests/test_future/test_utils.py
+++ tests/test_future/test_utils.py
@@ -382,7 +382,7 @@ class CustomException(Exception):
             out = Exception.__str__(self)
             if hasattr(self, '__cause__') and self.__cause__ and hasattr(self.__cause__, '__traceback__') and self.__cause__.__traceback__:
                 out += '\n\nThe above exception was the direct cause of the following exception:\n\n'
-                out += ''.join(traceback.format_tb(self.__cause__.__traceback__) + ['{}: {}'.format(self.__cause__.__class__.__name__, self.__cause__)])
+                out += ''.join(traceback.format_tb(self.__cause__.__traceback__) + ['{}: {}'.format(self.__cause__.__class__.__name__, self.__cause__.message)])
             return out
     else:
         pass
