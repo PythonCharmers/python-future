45cf38295a36fb8b59bd1921c926b940764492f3
Jordan Adler <jordan.m.adler@gmail.com>
2020-11-27 21:32:17 -0800

    Update docker image and parcel out to constant variable.  Add comment to update version constant

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 # XXX: TODO: we should make this include -e once tests pass
 set -xuo pipefail
 
-docker build . -t jmadler/python-future-builder
-docker push jmadler/python-future-builder:latest
-
+DOCKER_IMAGE=jmadler/python-future-builder
+# XXX: TODO: Perhaps this version shouldn't be hardcoded
 version=0.18.2
 
+docker build . -t $DOCKER_IMAGE
+docker push $DOCKER_IMAGE:latest
+
 for i in py26 py27 py33 py34 py35 py36 py37 py38 py39; do
-    docker run -ti -v $(realpath dist):/root/python-future/dist python-future-builder /root/python-future/setup.sh $version $(basename $i)
+    docker run -ti -v $(realpath dist):/root/python-future/dist $DOCKER_IMAGE /root/python-future/setup.sh $version $(basename $i)
 done
 
 python setup.py sdist
