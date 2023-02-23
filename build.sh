a914e33e9f48a77356006cd54e538d9d36ba8629
slycordinator <68940237+slycordinator@users.noreply.github.com>
2023-02-23 16:13:34 +0900

    Fixes according to output from shellcheck
    * Added "!#" to top of files that didn't have it.
    
    * Added quotes around variables/commands as recommended
    
    * Changed some to be /bin/sh instead of /bin/bash for portability
    * removed -o pipefail, especiallly because the scripts seem to not have any piped commands, but also due to it being a bashism

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
+#!/bin/sh
 # XXX: TODO: we should make this include -e once tests pass
-set -xuo pipefail
+set -xu
 
 DOCKER_IMAGE=jmadler/python-future-builder
 # XXX: TODO: Perhaps this version shouldn't be hardcoded
 version=0.18.3
 
-docker build . -t $DOCKER_IMAGE
-docker push $DOCKER_IMAGE:latest
+docker build . -t "$DOCKER_IMAGE"
+docker push "$DOCKER_IMAGE:latest"
 
 for i in py26 py27 py33 py34 py35 py36 py37 py38 py39; do
-    docker run -ti -v $(realpath dist):/root/python-future/dist $DOCKER_IMAGE /root/python-future/setup.sh $version $(basename $i)
+    docker run -ti -v "$(realpath dist)":/root/python-future/dist "$DOCKER_IMAGE" /root/python-future/setup.sh "$version" "$i"
 done
 
 python setup.py sdist
